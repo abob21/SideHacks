0a6e70b74aa742ad7859b1d5865c3b961dcd8b84
Callistus Ndemo <cnogoi@ncsu.edu>
2015-12-29 17:24:05 -0500

    Attempt to generate only even Fibonacci numbers.

--- src/FibonaccciEvenSum.java
+++ src/FibonaccciEvenSum.java
@@ -12,6 +12,20 @@ public class FibonaccciEvenSum {
 		
 		//Even numbers occur spaced about after two elements in the Fibonacci sequence
 		//1, (2), 3, 5, (8), 13, 21, (34), 55, 89, (144), 233, 377, (610)
+		//Generating only even numbers will make it easier. Do I need to use an array? 
+		//How do I keep track the the even number generated is less than 4000000
+		
+		/*Pseudocode for the project
+		 * sum = 0; 
+		 * 
+		 * Generate Fibonacci numbers
+		 * 	While (numbers <= 40000000) Boolean 
+		 * 		if (fn % 2 == 0) 
+		 * 			(sum = sum + fn) 
+		 * 		else 
+		 * 			
+		 * 
+		 * */
 	
 		
 			
